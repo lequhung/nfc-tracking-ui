38704f7151963c78bb35b00286364909d3f52495
Hung Le <hung.le@auspost.com.au>
2021-08-11 07:13:02 +1000

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,18 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`
       }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `NFC Tracking`,
+        short_name: `NFCT`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#0073e6`,
+        display: `standalone`,
+        icon: `src/images/favicon.png`
+      }
     }
   ]
 };
